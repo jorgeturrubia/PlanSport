3bbe302425060b61c644306f7ddc5f4c9e0d533b
jorge <jorgeturrubia@hotmail.com>
2025-08-15 10:03:13 +0200

    .

--- src/front/SportPlanner/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.sportplanner.com',
-  supabaseUrl: 'https://your-project.supabase.co',
-  supabaseAnonKey: 'your-production-anon-key'
-};
--- src/front/SportPlanner/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7061',
-  supabaseUrl: 'https://uyvcgsjrqgzpsbptmqxp.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
-};
